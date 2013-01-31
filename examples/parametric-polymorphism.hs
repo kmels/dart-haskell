ed56a485bef65103aa2af45db9d8385660e6b695
c.lopez <c.lopez@kmels.net>
2013-01-31 18:13:43 +0100

    Add example for parametric polymorphism
    Ignore-this: e8e118c986a658aadc17104569ad907e
    
    darcs-hash:20130131171343-c3db2-6e9169e887395bdaeb38fe4cda736fa99ff32656

---
+++ examples/parametric-polymorphism.hs
@@ -0,0 +1,16 @@
+-- Parametric polymorphism on lists
+
+module ParametricPolymorphism where
+
+import System.Environment
+
+concatenateIfGreaterThan :: [a] -> [a] -> Int -> [a]
+concatenateIfGreaterThan xs ys n = if ((length xs > n) && (length ys > n))
+                                   then xs ++ ys
+                                   else xs
+                                   
+
+main = getArgs >>= \args -> putStrLn $ case args of
+  [a,b] -> concatenateIfGreaterThan a b 2
+  _ -> error "Two string parameters only please."
+  
