d2ac5fdccce5d529574ed4c1de73832ea33a6be3
c.lopez <c.lopez@kmels.net>
2013-02-01 00:03:31 +0100

    Add function String -> String -> String
    Ignore-this: c01c6bbd5710b02475e83cbad242a6d3
    
    darcs-hash:20130131230331-c3db2-c1a2dfab807a47e831373c9f23ffe488fdcc032a

--- examples/string-concatenation.hs
+++ examples/string-concatenation.hs
@@ -5,8 +5,13 @@ import System.Environment
 main = do
   [arg] <- getArgs
   putStrLn $ myString arg
+  putStrLn $ myString' arg arg
 
 myString w = if (length w >= 2) 
              then "hello" ++ w
              else error "word can't be of length less than 2"
              
+
+myString' w w2 = if (length w >= 2 && length w2 >= 2)
+             then "hello" ++ w ++ w2
+             else error "word can't be of length less than 2"
