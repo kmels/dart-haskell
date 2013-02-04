61be99aa641435b9f77f7d432a5749d44420ebe1
c.lopez <c.lopez@kmels.net>
2013-02-04 21:03:05 +0100

    adds first example for the interpreter
    Ignore-this: d30c9b7ca50d992cd589bd3cddf3c8b6
    
    darcs-hash:20130204200305-c3db2-84f62886bacfe7e9e25eb007e5c98eba99cda074

---
+++ examples/interpreter/first-order-functions.hs
@@ -0,0 +1,19 @@
+
+sum :: Int -> Int -> Int
+a `sum` b = a + b
+
+isOdd :: Int -> Bool
+isOdd n = n `mod` 2 == 0
+
+intListLength :: [Int] -> Int
+intListLength [] = 0
+intListLength (x:xs) = intListLength xs + 1
+
+--eval
+fifty = intListLength [2,4..100]
+isOneOdd = isOdd 1
+
+main :: IO ()
+main = do
+  putStrLn . show $ fifty
+  putStrLn . show $ isOneOdd
