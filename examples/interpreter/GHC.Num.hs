4432a7a27754f52047da6db71b0024f04c32daae
Carlos Lopez-Camey <c.lopez@kmels.net>
2013-03-03 22:53:18 +0100

    adds GHC.Num.hs

---
+++ examples/interpreter/GHC.Num.hs
@@ -0,0 +1,32 @@
+module FirstOrderFunctions (
+  -- computed with Num operations
+    numberTen
+  , numberEleven
+  , numberTwenty
+  , numberTwentyTwo
+  , numberFourtyTwo
+  -- predicates on Nums
+  , isTenEven
+  , isHundredLargerThanZero
+  ) where
+
+-- Operatinons on Num
+plusOneInt :: Int -> Int --TODO, remove type signature
+plusOneInt n = n + 1
+twice :: Int -> Int --TODO, remove type signature
+twice n = n * 2
+
+-- Computed with Num operations
+numberTen = plusOneInt 9 -- DONE
+numberEleven = plusOneInt numberTen
+numberTwenty = twice $ numberTen -- DONE
+numberTwentyTwo = twice numberEleven -- DONE
+numberFourtyTwo = twice $ (twice 5) * 2 + 1 -- DONE
+
+-- Predicates
+
+isTenEven = isEven numberTen
+isHundredLargerThanZero = 100 >= 2
+
+isEven n = n `mod` 2 == 0
+
