09520d55274b0af51a688d62c68569189c0c4c6f
Carlos Lopez-Camey <c.lopez@kmels.net>
2013-04-17 14:58:02 +0200

    Compiler.JIT#mkName: find out names for potentially deleted (rewritten) definitions by GHC, recursive definitions those are

--- src/DART/Compiler/JIT.hs
+++ src/DART/Compiler/JIT.hs
@@ -19,12 +19,15 @@ import DART.FileIO
 import DART.InterpreterSettings
 import Language.Core.Core
 import Language.Core.Interpreter.Structures
+import Language.Core.Vdefg(vdefgNames, findVdef)
+import Text.Regex.Posix
+  
 -- | A function that receives an expression of the form `exp`,
 -- creates a haskell file with one definition, myDefinition = exp,
 -- compiles it to external core, parses it, and possibly returns a 
 -- single value definition
 jitCompile :: HaskellExpression -> IM (Maybe Vdefg)
-jitCompile (HaskellExpression expression_string _) = do
+jitCompile (HaskellExpression expression_string menv@(Module _ _ env_defs)) = do
   -- are we verbose?
   stgs <- gets settings
   let ?be_verbose = verbose stgs
@@ -33,10 +36,22 @@ jitCompile (HaskellExpression expression_string _) = do
   io $ writeFile "dart-jit.hs" (mkContent expression_string)
   modl <- io $ readModule "dart-jit.hs"
   case modl of
-    Module _ _ [vdefg] -> return . Just $ vdefg
-    Module _ _ vdefs  -> debugM "Something wrong happened"
-                         >> debugM ("Here are the value definitions" ++ show vdefs)
-                         >> return Nothing
+    Module _ _ vdefs  -> do
+       debugM $ "JIT-Compiled module, looking for the definition of " ++ def_name
+       return $ findVdef modl def_name
   where
+    def_name :: String
+    def_name = "jitDef"
     mkContent :: String -> String
-    mkContent exp = "module DART where\njitDef = " ++ expression_string
+    mkContent exp = "module DART where\n" ++ def_name ++ " = " ++ expression_string ++ content_env
+    content_env :: String
+    content_env = concatMap mkUndefined env_defs
+    mkUndefined :: Vdefg -> String
+    mkUndefined vdefg = concatMap (\name -> "\n" ++ mkName name ++ " = undefined") $ vdefgNames vdefg
+    mkName :: String -> String
+    -- | If we defined `fib`, ghc might have compiled this to e.g. `fibreZZ`
+    -- or `twice` to `twicereX`. We try to catch these definitions
+    mkName name = case (name =~ "(.*?)re" :: [[String]]) of
+      [] -> name
+      [m] -> last m
+      _ -> error "The impossible happened"
