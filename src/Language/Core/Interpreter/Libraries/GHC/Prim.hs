c53c2b4503474d0c229eaf645bdf8dc719a19443
Carlos Lopez-Camey <c.lopez@kmels.net>
2013-05-19 23:08:25 +0200

    Adds definition for GHC.Prim.>=#, fixes #29

--- src/Language/Core/Interpreter/Libraries/GHC/Prim.hs
+++ src/Language/Core/Interpreter/Libraries/GHC/Prim.hs
@@ -13,15 +13,45 @@
 
 module Language.Core.Interpreter.Libraries.GHC.Prim(all) where
 
+import Language.Core.Interpreter.Libraries.Monomophy(monomophy_2)
 import DART.CmdLine
 import Language.Core.Interpreter(evalId)
 import Language.Core.Interpreter.Structures
 import Prelude hiding (all)
 
 all :: [(Id, Either Thunk Value)]
-all = [ intSharpSum
+all = [ intSharpSum,
+        geqSharp
       ]
       
+-- | >=#, greater equals on ints
+geqSharp :: (Id, Either Thunk Value)
+geqSharp = (id, Right $ Fun (geq'_binary) "binary(>=#)")
+  where 
+    id = "ghc-prim:GHC.Prim.>=#"
+    geq'_binary :: Id -> Env -> IM Value
+    geq'_binary i e = evalId i e >>= \x -> return $ Fun (geq'_unary x) "unary(>=#)"
+      
+    geq'_unary :: Value -> Id -> Env -> IM Value
+    geq'_unary (Num x) i e = evalId i e >>= \val -> case val of
+      (Num y) -> return . Boolean $ x >= y
+      v -> return . Wrong $ "unary >=#, expected Int, got " ++ show val
+    geq'_unary v _ _  = return . Wrong $ "binary >=#, expected Int, got " ++ show v
+
+-- | >=#, greater equals on ints
+leqSharp :: (Id, Either Thunk Value)
+leqSharp = (id, Right $ Fun (geq'_binary) "binary(<=#)")
+  where 
+    id = "ghc-prim:GHC.Prim.<=#"
+    geq'_binary :: Id -> Env -> IM Value
+    geq'_binary i e = evalId i e >>= \x -> return $ Fun (geq'_unary x) "unary(<=#)"
+      
+    geq'_unary :: Value -> Id -> Env -> IM Value
+    geq'_unary (Num x) i e = evalId i e >>= \val -> case val of
+      (Num y) -> return . Boolean $ x <= y
+      v -> return . Wrong $ "unary <=#, expected Int, got " ++ show val
+    geq'_unary v _ _  = return . Wrong $ "binary <=#, expected Int, got " ++ show v
+        
 -- | +#, sums two ints
 intSharpSum :: (Id, Either Thunk Value)
 intSharpSum = (id, Right $ Fun sum'_1 "unary +#") where
@@ -32,5 +62,5 @@ intSharpSum = (id, Right $ Fun sum'_1 "unary +#") where
   sum'_2 :: Value -> Id -> Env -> IM Value
   sum'_2 (Num x) id env = evalId id env >>= \val -> case val of 
     (Num y) -> return . Num $ x + y
-    y -> return $ Wrong $ "binary I# expected an integer, got " ++ show y    
-  sum'_2 val _ _ = return $ Wrong $ "unary I#, expected an integer, got " ++ show val
+    y -> return $ Wrong $ "unary I#: expected an integer, got " ++ show y    
+  sum'_2 val _ _ = return $ Wrong $ "binary I#, expected an integer, got " ++ show val
